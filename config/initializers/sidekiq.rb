c2e2ad191315533500d7fafa8bbb4999e0eedd76
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2024-07-24 23:29:58 +0200

    fix: Override Sidekiq and logs redis options

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,18 @@
 # frozen_string_literal: true
 
+Sidekiq.class_eval do
+  def self.redis=(hash)
+    Rails.logger.warn("Sidekiq.redis=-------------------------")
+    Rails.logger.warn(hash)
+    @redis = if hash.is_a?(ConnectionPool)
+               hash
+             else
+               Sidekiq::RedisConnection.create(hash)
+             end
+    Rails.logger.warn("Sidekiq.redis=-------------------------EOL")
+  end
+end
+
 Sidekiq.configure_client do |config|
   config.redis = {
     network_timeout: ENV.fetch("REDIS_NETWORK_TIMEOUT", 10),
